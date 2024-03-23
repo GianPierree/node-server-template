63871a6519973e9c45494703611e5f4c4d346ce6
Gian Munoz <gian0291@gmail.com>
2024-03-23 10:28:32 -0500

    feat: :zap: Se agrego el carpeta de services y se agrego la variable B24_hook

--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,5 @@ require('dotenv').config()
 module.exports = {
     ENV: process.env.ENV || 'dev',
     PORT: process.env.PORT || 8080,
+    B24_HOOK: process.env.B24_HOOK || 'https://www.test.com'
 }
